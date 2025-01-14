2c2804fb70a134b91077c30a629a7fd20da99ec9
sherm <shermawns@gmail.com>
2025-01-14 10:39:21 -0300

    feat: first commit

---
+++ src/main/java/com/BuildingMaintenance/BuildingMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.BuildingMaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingMaintenanceApplication.class, args);
+	}
+
+}
